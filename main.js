f677f7d286817628be4cd0eb4b1ab9f86a1d7e39
Babak Ghaffari <100711307+Babak01Gh@users.noreply.github.com>
2023-04-12 12:58:17 +0330

    add files

---
+++ main.js
@@ -0,0 +1,22 @@
+
+const submitBtn = document.querySelector('input[type="submit"]');
+const detailsContainer = document.querySelector('.details-for');
+
+submitBtn.addEventListener('click', ()=>{
+    const selected = document.querySelector('input[name="email"]:checked').id;
+    detailsContainer.classList.add('Onload');
+    getDetails(selected,(data)=>{
+        detailsContainer.innerHTML = `
+        <p class="email">${data.email}</p>
+        <p class="detail">${data.loginDetails}</p>
+        <p class="courses">${data.courses}</p>
+        `
+        detailsContainer.classList.remove('Onload');
+    })
+})
+
+function getDetails(id,callback){
+    setTimeout(()=>{
+        axios.get(`http://localhost:3000/emails/${id}`).then(user => callback(user.data));    
+    },2000)
+}
